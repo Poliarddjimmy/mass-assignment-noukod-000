c0c9b5e3c5dfb4309d5af0df5addad4ab1f73fa2
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-04-29 20:25:49 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,7 +2,7 @@ class Person
   #your code here
   attr_accessor :name, :birthday,:hair_color, :eye_color,:height,:weight, :handed,:complexion,:t_shirt_size, :wrist_size, :glove_size,:pant_length, :pant_width
   
-  def initialize (name:, birthday:,hair_color:, eye_color:,height: 5,weight: 5, handed:,complexion:,t_shirt_size: 5, wrist_size: 5, glove_size: 5,pant_length: 5, pant_width: 5)
+  def initialize (name:, birthday:,hair_color:, eye_color:,height: 5,weight: 5, handed:,complexion:,t_shirt_size: 5, wrist_size: 5, glove_size: nil,pant_length: nil, pant_width:nil)
     @name=name
     @birthday=birthday
     @hair_color=hair_color
