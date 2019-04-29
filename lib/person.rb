964901c67bd81ce57653bcac4f210fa4902954ec
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-04-29 20:21:10 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -6,6 +6,13 @@ class Person
     @name=name
     @birthday=birthday
     @hair_color=hair_color
-    @eye_color
+    @eye_color=eye_color
+    @height=height
+    @weight=weight
+    @handed=handed
+    @complexion=complexion
+    @t_shirt_size=t_shirt_size
+    @wrist_size=wrist_size
+    @glove_sizegl
   end
 end
